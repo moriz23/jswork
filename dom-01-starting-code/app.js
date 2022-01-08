d1fc37630d817b6e59c631221c5afbdec24c6f4c
Mohammed Rizvi <morizvi07@gmail.com>
2022-01-08 13:41:41 -0500

    worked on functions'

--- dom-01-starting-code/app.js
+++ dom-01-starting-code/app.js
@@ -1,5 +1,18 @@
 const section = document.querySelector('section');
 const button = document.querySelector('button');
+const div = document.querySelector('div');
+const list = document.querySelector('ul');
+
+const newLi = document.createElement('li');
+list.append(newLi);
+newLi.textContent = 'Item 4';
+
+const newerLi = document.createElement('li');
+const listItem2 = list.children[1];
+newerLi.textContent = 'Item 2.1';
+listItem2.insertAdjacentElement('afterend', newerLi);
+
+const newUl = list.cloneNode(true);
 
 button.addEventListener('click', () => {
   // if (section.className === 'red-bg visible') {
@@ -13,4 +26,5 @@ button.addEventListener('click', () => {
   // }
 
   section.classList.toggle('invisible');
+  div.insertAdjacentHTML('beforeend', '<p>Something went wrong!</p>');
 });
--- exercise-4/assignment.js
+++ exercise-4/assignment.js
@@ -1,3 +1,48 @@
 // function sayHello(name) {
 //   console.log('Hi ' + name);
 // }
+const FALLBACK = 'John';
+
+const sayHello = (name = FALLBACK) => {
+  console.log('Hi ' + name);
+};
+
+const sayHello2 = (greeting, name = FALLBACK) => {
+  console.log(`${greeting} ${name}`);
+};
+
+const sayHello3 = () => {
+  console.log('How is it going mo money?');
+};
+
+const sayHello4 = (value) => {
+  return value;
+};
+
+const checkInput = (cb, ...texts) => {
+  let hasEmptyText = false;
+
+  for (const text of texts) {
+    if (!text) {
+      hasEmptyText = true;
+      break;
+    }
+  }
+
+  if (!hasEmptyText) {
+    cb();
+  }
+};
+
+const checkingInputs = () => {
+  console.log('All not empty');
+};
+
+sayHello('Mohammed');
+sayHello();
+sayHello2('What up', 'Mohammed');
+sayHello2('Salutations');
+const name = sayHello4('Mohammed');
+console.log(`hey what up ${name}`);
+
+const string = checkInput(checkingInputs, 'hey', 'dude', 'hey yo', 'hello');
